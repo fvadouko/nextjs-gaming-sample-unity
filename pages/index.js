c9c7c0fe0f45d26e3d64bfc749b4801203efc1e0
ADOUKO Franck Venance <fvadouko@live.fr>
2022-03-09 10:52:56 +0000

    renaming the folders

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import api from "../auth/axios";
+import api from "../lib/axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
