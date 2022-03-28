18eeb3302100844bdb97acda0cfd51ff25bf1976
ADOUKO Franck Venance <fvadouko@live.fr>
2022-03-28 20:15:41 +0200

    fix bug about unescaped-entities

--- pages/voir-panier.js
+++ pages/voir-panier.js
@@ -73,7 +73,7 @@ const VoirPanier = () => {
                 }
               }}
             >
-              S\'abonner
+              Abonnez-vous
             </button>
           </div>
         </div>
